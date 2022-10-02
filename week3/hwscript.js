e16550f73514022fe0b04ebb1ede38a528178710
jacey880808 <chungyuchieh19990808@gmail.com>
2022-10-02 16:11:15 -0700

    Update hwscript.js

--- week3/hwscript.js
+++ week3/hwscript.js
@@ -1,12 +1,12 @@
-let imgButton = document.getElementById("imageBtn")
-console.log(imgButton)
-
-const addAnImage = ()=>{
-    let imgElem = document.createElement("img")
-    imgElem.src = "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e0/SNice.svg/1920px-SNice.svg.png"
-    imgElem.alt = "smily face"
-
-    mnElement.appendChild(imgElem)
+function background1(){
+    document.querySelector('body').style.background = "url('01kin.JPG') center center / cover";
 }
-
-imgButton.addEventListener("click", addAnImage)
+function background2(){
+    document.querySelector('body').style.background = "url('02kin.JPG') center center / cover";
+}
+function background3(){
+    document.querySelector('body').style.background = "url('03kin.JPG') center center / cover";
+}
+function background4(){
+    document.querySelector('body').style.background = "url('04kin.JPG') center center / cover";
+}

030c77ded8ed91c157fea64ea139d129095f41e0
jacey880808 <chungyuchieh19990808@gmail.com>
2022-12-19 13:49:38 -0800

    Update hw.js

--- week9/hw.js
+++ week9/hw.js
@@ -4,14 +4,19 @@ let detector;
 let detections = [];
 
 function preload() {
-  detector = ml5.objectDetector("cocossd");
+  detector = ml5.objectDetector('cocossd', modelReady);
 }
+
 
 function gotDetections(error, results) {
   if (error) {
     console.error(error);
   }
   detections = results;
+  detector.detect(video, gotDetections);
+}
+
+function modelReady() {
   detector.detect(video, gotDetections);
 }
 
